2167b80e66367da0ef82d614163391f796625fde
JoshueLobo19 <josuespencerrojas@gmail.com>
2023-11-03 15:54:27 -0500

    cambios del viernes 3/11

--- auth-service/src/main/java/com/microservice/authservice/models/dao/IPruebaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.authservice.models.dao;
-
-import com.microservice.authservice.models.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPruebaDAO extends JpaRepository<Usuario,Long> {
-}
